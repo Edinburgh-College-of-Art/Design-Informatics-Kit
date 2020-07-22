698af1b4555a7214f5d42026b40ffcc79898958e
mhamilt <matt.hamilton@ed.ac.uk>
2020-07-22 12:12:32 +0100

    if to switch

--- example-projects/wifi/API_request_template/WiFi_Utils.ino
+++ example-projects/wifi/API_request_template/WiFi_Utils.ino
@@ -2,29 +2,25 @@
 
 void connectToWifi(const char ssidName[], const char pass[])
 {
-//#ifdef ARDUINO_SAMD_ZERO
-//  WiFi.setPins(8, 7, 4, 2); // Set the WiFi pins on the Feather
-//#endif
-
   Serial.println("Starting...");
   Serial.println("Connecting to WiFi.");
 
   int conn = WiFi.begin(ssidName, pass);
 
-  if (conn == WL_CONNECTED)
+  switch (conn)
   {
-    Serial.println("OK!");
-  }
-  else if (conn == WL_IDLE_STATUS)
-  {
-    Serial.println("Idle");
-  }
-  else
-  {
-    Serial.println("WiFi Connection Failed!");
-  }
+    case WL_CONNECTED:
+      Serial.println("OK!");
+      break;
+    case WL_IDLE_STATUS:
+      Serial.println("Idle");
+      break;
+    default:
+      Serial.println("WiFi Connection Failed!");
 
+  }
 }
+
 void haltFirmware()
 {
   Serial.println("\n\nSTOPPING THE PROGRAM!");
